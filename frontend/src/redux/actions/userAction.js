0c62983366a805d0a712bf3c4763429be813382d
dendinardiansyahsaputra <91442121+GROWDEN@users.noreply.github.com>
2023-07-20 19:50:08 +0700

    Update userAction.js

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -15,10 +15,12 @@ import {
   USER_SIGNUP_SUCCESS,
 } from "../constants/userConstant";
 
+const API_BASE_URL = "https://project-tugas-akhir.vercel.app";
+
 export const userSignInAction = (user) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST });
   try {
-    const { data } = await axios.post("/api/signin", user);
+    const { data } = await axios.post(`${API_BASE_URL}/api/signin`, user);
     localStorage.setItem("userInfo", JSON.stringify(data));
     dispatch({
       type: USER_SIGNIN_SUCCESS,
@@ -38,7 +40,7 @@ export const userSignInAction = (user) => async (dispatch) => {
 export const userSignUpAction = (user) => async (dispatch) => {
   dispatch({ type: USER_SIGNUP_REQUEST });
   try {
-    const { data } = await axios.post("/api/signup", user);
+    const { data } = await axios.post(`${API_BASE_URL}/api/signup`, user);
 
     dispatch({
       type: USER_SIGNUP_SUCCESS,
@@ -59,7 +61,7 @@ export const userLogoutAction = () => async (dispatch) => {
   dispatch({ type: USER_LOGOUT_REQUEST });
   try {
     localStorage.removeItem("userInfo");
-    const { data } = await axios.get("/api/logout");
+    const { data } = await axios.get(`${API_BASE_URL}/api/logout`);
     dispatch({
       type: USER_LOGOUT_SUCCESS,
       payload: data,
@@ -78,7 +80,7 @@ export const userLogoutAction = () => async (dispatch) => {
 export const userProfileAction = () => async (dispatch) => {
   dispatch({ type: USER_LOAD_REQUEST });
   try {
-    const { data } = await axios.get("/api/me");
+    const { data } = await axios.get(`${API_BASE_URL}/api/me`);
     dispatch({
       type: USER_LOAD_SUCCESS,
       payload: data,
